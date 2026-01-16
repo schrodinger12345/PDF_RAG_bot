94557151de73ef79a8311e9d52949f8412348556
Aryan Tambe <aryanstambe2005@gmail.com>
2026-01-16 18:08:40 +0530

    80% backend completed , fixed some inconsistencies across the project directory

--- data_loader.py
+++ data_loader.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-EMBED_MODEL = "gemini-pro-embedding-002"
+EMBED_MODEL = "gemini-embedding-001"  # Replace with a valid model name
 EMBED_DIM = 3072
 
 splitter = SentenceSplitter(chunk_size=1000, chunk_overlap=200)
@@ -22,10 +22,12 @@ def load_and_chunk_pdfs(path:str):
         chunks.extend(split_texts)
     return chunks
 
+
 def get_embeddings(texts:list[str]) -> list[list[float]]:
-    response = client.embeddings.create(
-        model = EMBED_MODEL,
-        input = texts
+    response = client.models.embed_content(
+        model=EMBED_MODEL,
+        contents=texts
     )
-    embeddings = [item.embedding for item in response.data]
+    print(dir(response.embeddings[0]))
+    embeddings = [item.values for item in response.embeddings]
     return embeddings
--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from custom_types import RAGChunkAndSRC, RAGUpsertResponse, RAGSearchResponse, R
 
 load_dotenv()
 
+
+
 inngest_client = inngest.Inngest(
     app_id="RAGpdfApp",
     logger=logging.getLogger("uvicorn"),
@@ -25,8 +27,48 @@ inngest_client = inngest.Inngest(
     fn_id= "RAG : Ingest PDF",
     trigger= inngest.TriggerEvent(event="rag/ingest-pdf")
 )
+
+
 async def ingest_pdf(ctx:inngest.Context):
-    return {"status": "Api is working!"}
+    def _load(ctx: inngest.Context) -> RAGChunkAndSRC:
+        pdf_path = ctx.event.data.get("pdf_path")
+        source_id = ctx.event.data.get("source_id", pdf_path)
+        chunks = load_and_chunk_pdfs(pdf_path)
+        return RAGChunkAndSRC(
+            source_id= source_id,
+            pdf_chunks= chunks
+        )
+
+    def _upsert(ChunkAndSrc: RAGChunkAndSRC)-> RAGUpsertResponse:
+        chunks = ChunkAndSrc.pdf_chunks
+        source_id = ChunkAndSrc.source_id
+        vecs = get_embeddings([chunk for chunk in chunks])
+        ids = [str(uuid.uuid4()) for _ in range(len(chunks))]
+        payloads = [
+            {
+                "source_id": source_id,
+                "text": chunk,
+            }
+            for chunk in chunks
+        ]
+        qdrrant = QdrantStorage().upsert(ids, vecs, payloads)
+        return RAGUpsertResponse(ingested=len(ids))
+
+    chunk_and_src = await ctx.step.run("Loading and chunking PDFs", lambda: _load(ctx=ctx), output_type=RAGChunkAndSRC)
+    inngested = await ctx.step.run("Embedding and upserting", lambda: _upsert(ChunkAndSrc=chunk_and_src), output_type=RAGUpsertResponse)
+    return inngested.model_dump()
+
+@inngest_client.fast_api.create_function(
+    fn_id="RAG : Search PDF",
+    trigger=inngest.TriggerEvent(event="rag/search-pdf")
+)
+async def rag_query_pdf_ai(ctx: inngest.Context):
+    def _search(question: str , top_k: int =5) -> RAGSearchResponse:
+        query_vec = get_embeddings([question])[0]
+        store = QdrantStorage()
+        res = store.search(query_vector=query_vec, top_k=top_k)
+        return RAGSearchResponse(context=res["context"], sources=res["sources"])
+
 
 app = FastAPI()
 
